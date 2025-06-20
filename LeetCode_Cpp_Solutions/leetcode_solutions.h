c5675fd19d2e19b6280eb6c0de99f14e279b9b12
ExpIMD <fourexpimd@gmail.com>
2025-06-20 12:57:32 +0300

    Solving the <7. Reverse Number> Problem

--- LeetCode_Cpp_Solutions/leetcode_solutions.h
+++ LeetCode_Cpp_Solutions/leetcode_solutions.h
@@ -1,10 +1,10 @@
 #ifndef __LEETCODE_SOLUTIONS_
 #define __LEETCODE_SOLUTIONS_
 
+#include <optional>
+#include <string>
 #include <tuple>
 #include <vector>
-#include <string>
-#include <optional>
 
 // Given a vector of integers <numbers> and an integer <target>
 // Returns a tuple of two indices of elements in a vector whose sum equals the <target>
@@ -27,10 +27,11 @@ size_t search_insert_position(const std::vector<T>& vec, T target) {
 		// We shift the left and right pointers
 		if (target < vec[middle]) right = middle;
 		else if (vec[middle] < target) left = middle + 1;
-		else return middle; // Exist solution
+		else return middle; // Found solution
 	}
 	return left; // Returns the index in the vector where the target would theoretically be positioned 
 }
+
 // Given a sorted vector <vec> and a value <target>
 // Returns the index of <target> in <vec> if it exists
 // Otherwise, returns -1
@@ -47,9 +48,34 @@ std::optional<size_t> binary_search(const std::vector<T>& vec, T target) {
 		// We shift the left and right pointers
 		if (target < vec[middle]) right = middle;
 		else if (vec[middle] < target) left = middle + 1;
-		else return middle; // Exist solution
+		else return middle; // Found solution
 	}
-	return std::nullopt; // Returns the index in the vector where the target would theoretically be positioned 
+	return std::nullopt; // Returns std::nullopt if the target element is not found
+}
+
+// Given an integer <number>
+// Returns the reversed <number> with preservation of sign
+// Returns zero in case of value overflow
+template<typename T> requires std::integral<T>
+T reversed(T number) {
+	T reversed{ 0 };
+
+	while (number != 0) {
+		T pop = number % 10; // Write down the last digit of the number
+
+		// Checks for value overflow
+		if (reversed > std::numeric_limits<T>::max() / 10 ||
+			(reversed == std::numeric_limits<T>::max() / 10 && pop > std::numeric_limits<T>::max() % 10))
+			return 0;
+		if (reversed < std::numeric_limits<T>::min() / 10 ||
+			(reversed == std::numeric_limits<T>::min() / 10 && pop < std::numeric_limits<T>::min() % 10))
+			return 0;
+
+		// Construct the reversed number and change the number
+		reversed = reversed * 10 + pop;
+		number /= 10;
+	}
+	return reversed; // Return the reversed number
 }
 
 #endif __LEETCODE_SOLUTIONS_
--- LeetCode_Cpp_Solutions/program.cpp
+++ LeetCode_Cpp_Solutions/program.cpp
@@ -5,8 +5,8 @@
 
 int main() {
 
-	std::vector<int> vec{ -2, 0, 1, 4, 7 };
-	std::cout << std::boolalpha << binary_search(vec, 7).has_value();
+	short value = 32767;
+	std::cout << reversed(value);
 
 	return 1;
 }
