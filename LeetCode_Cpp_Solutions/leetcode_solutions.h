e465cd0b4eaffe744729302f7755d272c123a7e8
ExpIMD <fourexpimd@gmail.com>
2025-06-20 14:17:36 +0300

    Solving the <58. Length of Last Word> Problem

--- LeetCode_Cpp_Solutions/leetcode_solutions.h
+++ LeetCode_Cpp_Solutions/leetcode_solutions.h
@@ -78,4 +78,15 @@ T reversed(T number) {
 	return reversed; // Return the reversed number
 }
 
+// Given a string <line>
+// Returns the length of the last word in <line>
+size_t last_word_length(const std::string& line) {
+	size_t length{ 0 };
+	for (size_t i{ line.size() }; i-- > 0; ) { // This unusual construction is needed to avoid value overflow and 
+		if (!std::isspace(line[i])) ++length;
+		else if (length > 0) break; // Verifying whether a word has been seen before
+	}
+	return length;
+}
+
 #endif __LEETCODE_SOLUTIONS_
